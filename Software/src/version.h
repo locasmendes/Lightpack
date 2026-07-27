ef39cf7b5e96f59c24534d784598e3ab10b86584
Lucas Mendes <lukinhazmm@gmail.com>
2026-07-27 05:45:02 -0300

    Bump version to 5.14.0.0
    Constant-color / mood-lamp smoothing fix, wizard LED-cap fix, and
    serial port autodetect (fix/moodlamp-smoothing-wizard-usb) merged.

--- Software/src/version.h
+++ Software/src/version.h
@@ -27,10 +27,10 @@
 #pragma once
 
 // Current version of Lightpack software
-#define VERSION_STR "5.13.0.0"
+#define VERSION_STR "5.14.0.0"
 
 // Resource defines using in Lightpack.rc
-#define VER_FILEVERSION				5,13,0,0
+#define VER_FILEVERSION				5,14,0,0
 #define VER_PRODUCTVERSION			VER_FILEVERSION
 
 #define VER_FILEVERSION_STR			VERSION_STR "\0"
